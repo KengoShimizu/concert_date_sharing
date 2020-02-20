c0f0779dadff86881c586c5522fb61338a6f207c
清水 謙吾 <qwerty0308kengo@gmail.com>
2020-02-20 18:11:46 +0900

    fin the development back system of the forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './sass/form.scss';
 import { Button, TextField, Typography } from '@material-ui/core';
 import { MuiPickersUtilsProvider, InlineDatePicker } from "material-ui-pickers";
 import DateFnsUtils from "@date-io/date-fns";
@@ -13,82 +14,105 @@ class Form extends React.Component {
       todohukens: [],
       kaijos: [],
       inputs: [],
-      selectedDates: []
+      selectedDates: [],
+      isinputted: false
     };
   }
 
   // dateがpickされたときに実行される関数
-  handleDateChange = date => {
-    // 日付+indexの文字列になっているため，分割する
-    const index = date[date.length-1];
-    const date_ = date.substring( 0, date.length-1 );
-
+  handleDateChange = (date, index) => {
     // stateの配列を更新するためコピー先の配列全体を変更する
     const selectedDates_copy = this.state.selectedDates.slice();
-    selectedDates_copy[index] = date_;
+    selectedDates_copy[index] = date;
     this.setState({ selectedDates: selectedDates_copy });
   };
 
-  handleChange_concert = e => {
-    const index = e.target.id.split(' ')[0].replace('index', '');
-
+  handleChange_concert = (e, index) => {
     // stateの配列を更新するためコピー先の配列全体を変更する
     const concerts_copy = this.state.concerts.slice();
     concerts_copy[index] = e.target.value;
-    this.setState({ concerts: concerts_copy });
+
+    // this.setStateの即時反映には，コールバックを使う
+    this.setState({ concerts: concerts_copy }, () => {
+      // 全ての情報が記入されていたらsubmitを許す
+      if(this.state.concerts.indexOf('') === -1 &&
+         this.state.todohukens.indexOf('') === -1 &&
+         this.state.kaijos.indexOf('') === -1){
+           this.setState({ isinputted: true});
+      }
+      else{
+        this.setState({ isinputted: false});
+      }
+    });
   }
 
-  handleChange_todohuken = e => {
-    // 都道府県プルダウン操作した時
-    let index;
-    if (e.target.id){
-      index = e.target.id.split(' ')[0].replace('index', '');
-    }
-    // 右のxボタンで都道府県名を消した場合
-    else{
-      index = e.target.parentElement.parentElement.parentElement.parentElement.querySelector('input').id.split(' ')[0].replace('index', '');
-    }
-    const str = e.target.innerHTML[0] == '<' ? '' : e.target.innerHTML;
+  handleChange_todohuken = (e, index) => {
+    const str = e.target.innerHTML[0] === '<' ? '' : e.target.innerHTML;
 
     // stateの配列を更新するためコピー先の配列全体を変更する
     const todohukens_copy = this.state.todohukens.slice();
     todohukens_copy[index] = str;
-    this.setState({ todohukens: todohukens_copy });
+    this.setState({ todohukens: todohukens_copy }, () => {
+      // 全ての情報が記入されていたらsubmitを許す
+      if(this.state.concerts.indexOf('') === -1 &&
+         this.state.todohukens.indexOf('') === -1 &&
+         this.state.kaijos.indexOf('') === -1){
+           this.setState({ isinputted: true});
+      }
+      else{
+        this.setState({ isinputted: false});
+      }
+    });
+
   }
 
-  handleChange_kaijo = e => {
-    const index = e.target.id.split(' ')[0].replace('index', '');
-
+  handleChange_kaijo = (e, index) => {
     // stateの配列を更新するためコピー先の配列全体を変更する
     const kaijos_copy = this.state.kaijos.slice();
     kaijos_copy[index] = e.target.value;
-    this.setState({ kaijos: kaijos_copy });
+    this.setState({ kaijos: kaijos_copy }, () => {
+      // 全ての情報が記入されていたらsubmitを許す
+      if(this.state.concerts.indexOf('') === -1 &&
+         this.state.todohukens.indexOf('') === -1 &&
+         this.state.kaijos.indexOf('') === -1){
+           this.setState({ isinputted: true});
+      }
+      else{
+        this.setState({ isinputted: false});
+      }
+    });
+
   }
 
   // 「公演情報を追加する」ボタンが押された時に実行される関数
   append_details = () => {
+    console.log(this.state.inputs);
     const artist_isinputted = this.state.artist;
     const newInput = this.state.inputs.length;
     // アーティスト名が入力されているか
     if (artist_isinputted){
-      if (newInput == 0){
+      if (newInput === 0){
         this.setState(prevState => ({
           inputs: prevState.inputs.concat([newInput]),
           concerts: prevState.concerts.concat(['']),
           todohukens: prevState.todohukens.concat(['']),
           kaijos: prevState.kaijos.concat(['']),
-          selectedDates: prevState.selectedDates.concat([new Date()])
+          selectedDates: prevState.selectedDates.concat([new Date()]),
+          isinputted: false
         }));
       }
       else{
         // 全ての情報が記入されていたら公演情報追加を許す
-        if(this.state.concerts.indexOf('') == -1 && this.state.todohukens.indexOf('') == -1 && this.state.kaijos.indexOf('') == -1){
+        if(this.state.concerts.indexOf('') === -1 &&
+           this.state.todohukens.indexOf('') === -1 &&
+           this.state.kaijos.indexOf('') === -1){
           this.setState(prevState => ({
             inputs: prevState.inputs.concat([newInput]),
             concerts: prevState.concerts.concat(['']),
             todohukens: prevState.todohukens.concat(['']),
             kaijos: prevState.kaijos.concat(['']),
-            selectedDates: prevState.selectedDates.concat([new Date()])
+            selectedDates: prevState.selectedDates.concat([new Date()]),
+            isinputted: false
           }));
         }
         else{
@@ -101,6 +125,31 @@ class Form extends React.Component {
       // requiredを誘発するためsubmit()ではなくclick()
       document.getElementById('submit').click();
     }
+  };
+
+  delete_details = index => {
+    this.state.inputs.splice(index, 1);
+    this.state.concerts.splice(index, 1);
+    this.state.todohukens.splice(index, 1);
+    this.state.kaijos.splice(index, 1);
+    this.state.selectedDates.splice(index, 1);
+    this.setState(prevState => ({
+      inputs: prevState.inputs,
+      concerts: prevState.concerts,
+      todohukens: prevState.todohukens,
+      kaijos: prevState.kaijos,
+      selectedDates: prevState.selectedDates
+    }), () => {
+      // 全ての情報が記入されていたらsubmitを許す
+      if(this.state.concerts.indexOf('') === -1 &&
+         this.state.todohukens.indexOf('') === -1 &&
+         this.state.kaijos.indexOf('') === -1){
+           this.setState({ isinputted: true});
+      }
+      else{
+        this.setState({ isinputted: false});
+      }
+    });
   };
 
   render() {
@@ -118,40 +167,46 @@ class Form extends React.Component {
             <br />
 
             {/* 公演情報を追加する」ボタンが押された時に順次追加*/}
-            {this.state.inputs.map(input =>
-              <div key={input}>
+            {this.state.inputs.map((v, i) =>
+              <div key={i}>
                 <br />
                 <Typography
                   variant="h6"
                   color="secondary"
                   gutterBottom>
-                  公演情報{input+1}
+                  公演情報{i+1}
+                  <Button
+                　  variant="contained"
+                  　color="primary"
+                  　onClick={() => this.delete_details(i)}>
+                    削除する
+                  </Button>
                 </Typography>
 
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
                   <InlineDatePicker
                     label="ライブ日程"
                     format="yyyy/MM/dd"
-                    onChange={date => this.handleDateChange(date+input)}
-                    value={this.state.selectedDates[input]}
+                    onChange={date => this.handleDateChange(date, i)}
+                    value={this.state.selectedDates[i]}
                   />
                 </MuiPickersUtilsProvider>
                 <br />
 
                 <TextField
                   label="ライブ名"
-                  id={`index${input} standard-basic`}
-                  onChange={this.handleChange_concert}
-                  value={this.state.concerts[input]}
+                  id={`index${i} standard-basic`}
+                  onChange={e => this.handleChange_concert(e, i)}
+                  value={this.state.concerts[i]}
                   required
                 />
                 <br />
 
                 <Autocomplete
-                  id={`index${input} combo-box-demo`}
+                  id={`index${i} combo-box-demo`}
                   options={options}
                   getOptionLabel={option => option.label}
-                  onChange={this.handleChange_todohuken}
+                  onChange={e => this.handleChange_todohuken(e, i)}
                   style={{ width: 300 }}
                   renderInput={params => (
                     <TextField {...params}
@@ -165,14 +220,14 @@ class Form extends React.Component {
 
                 <TextField
                   label="会場名"
-                  id={`index${input} standard-basic`}
-                  onChange={this.handleChange_kaijo}
-                  value={this.state.kaijos[input]}
+                  id={`index${i} standard-basic`}
+                  onChange={e => this.handleChange_kaijo(e, i)}
+                  value={this.state.kaijos[i]}
                   required
                 />
                 <br />
-              </div>)
-            }
+              </div>
+            )}
 
             <br />
             <Button
@@ -186,7 +241,9 @@ class Form extends React.Component {
               id="submit"
               variant="contained"
               color="primary"
-              type="submit">
+              className={this.state.isinputted ? "" : "submit-disabled"}
+              type="submit"
+              >
               登録する
             </Button>
           </form>
