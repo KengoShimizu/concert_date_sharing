b61e79f40b14050c16610158df2f04cf86b0ad4c
清水 謙吾 <qwerty0308kengo@gmail.com>
2020-03-02 16:39:16 +0900

    fix the db

--- src/components/Form.js
+++ src/components/Form.js
@@ -179,8 +179,8 @@ class Form extends React.Component {
       e.preventDefault();
       return false;
     }
-    //fetch('https://concertsharing.herokuapp.com/concert_details', {
-    fetch('http://localhost:3001/concert_details', {
+    fetch('https://concertsharing.herokuapp.com/concert_details', {
+    //fetch('http://localhost:3001/concert_details', {
       method: 'POST',
       body: JSON.stringify({
         artist: this.state.artist,
